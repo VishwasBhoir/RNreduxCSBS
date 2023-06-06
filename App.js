4d2cfaa8c4eefba3a436afbdde068b3940fb61ef
VISHWAS BHOIR <vishwas.11challengers@gmail.com>
2023-06-06 13:29:10 +0530

    Implemented Flatelist

--- App.js
+++ App.js
@@ -1,19 +1,71 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Image,
+  ScrollView,
+} from "react-native";
 import Header from "./components/Header";
-import Products from "./components/Products";
+import Product from "./components/Product";
 
 export default function App() {
+  const products = [
+    {
+      id: "1",
+      name: "Samsung S22",
+      color: "White",
+      price: "59,999.00",
+      imgURI: "./assets/img/SamsungS21.png",
+    },
+    {
+      id: "2",
+      name: "Iphone 14",
+      color: "Black",
+      price: "89,000.00",
+      imgURI: "./assets/img/Iphone.png",
+    },
+    {
+      id: "3",
+      name: "OnePlus",
+      color: "White",
+      price: "62,990.00",
+      imgURI: "./assets/img/OnePlus.png",
+    },
+  ];
+
+  const Item = ({ name, color, price, imgURI }) => (
+    <View style={{}}>
+      <Image
+        style={{ height: 200, width: 200 }}
+        source={require("./assets/img/OnePlus.png")}
+      />
+      <Text>{name}</Text>
+      <Text>{color}</Text>
+      <Text>{price}</Text>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
       <Header />
-      <Products />
+      <ScrollView>
+        <FlatList
+          data={products}
+          renderItem={({ item }) => (
+            <Item name={item.name} color={item.color} price={item.price} />
+          )}
+          keyExtractor={(item) => item.id}
+        />
+        <Product />
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 50,
-    margin: 10,
+    marginTop: 35,
+    margin: 5,
   },
 });
--- components/Product.js
+++ components/Product.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-const Products = () => {
+const Product = () => {
   return (
     <View style={styles.mainContainerStyle}>
       <Text style={styles.textStyle}>Products</Text>
@@ -9,14 +9,18 @@ const Products = () => {
   );
 };
 
-export default Products;
+export default Product;
 
 const styles = StyleSheet.create({
   mainContainerStyle: {
     backgroundColor: "#00C4FF",
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 10,
   },
 
   textStyle: {
     fontSize: 30,
+    padding: 10,
+    color: "white",
   },
 });
